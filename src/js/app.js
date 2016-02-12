514b6e08e6ee300f4c694efff95445671726ed9c
Paul Rose <prose100@hotmail.com>
2016-02-12 11:59:17 -0600

    The hero moves.  Start/stop controls are available.

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,86 @@
 (function() {
 
+  // "use strict";
+
   $(document).ready(function(){
 
     var defaults = {
       hero: 'hero',
       heroWidth: 50,
-      heroHeight: 50
+      heroHeight: 50,
+      stopClass: '.stop'
     };
 
     function ArcadeGame(element, options) {
     
       settings = $.extend({}, defaults, options);
-      $this = $(element);
-      this.init();
+
+      var $this = $(element); //start button
+      var that = this; //arcadegame
+      
+      $this.click(function(){
+        that.start();
+      })
     }
 
-    ArcadeGame.prototype.init = function() {
-      var hero = new Hero(3);
-      hero.draw();
-      var keyBoard = new Keyboard
+    ArcadeGame.prototype.start = function() {
+      hero = new Hero(1);
+      var targetElement = document.body;
+
+      targetElement.addEventListener('keydown', function (event) {
+           switch (event.keyCode) {
+            case 13:
+              //***fire bullet
+              break;
+            case 37:
+              hero.move('left');
+              break;
+            case 39:
+              hero.move('right');
+              break;
+           };
+        });
+     
+      var stop = setInterval(function() {
+          play();
+        }, 100); 
+
+      function play() {
+          if (gameIsOver()) {clearInterval(stop)};
+          $(".stop").click(function(){clearInterval(stop)});
+        
+          updateBoard();
+          //run sequence of steps to keep game going
+      }
+
+      //may put these functions in separate Arcade.prototype
+      function gameIsOver() {
+        return hero.isDead(); 
+      }
+
+      function updateBoard() {
+        hero.draw();
+      }        
     }
     
     function Position(x,y) {
-      this.x = x
-      this.y = y
+      this.x = x;
+      this.y = y;
+    }
+
+    Position.prototype.setPositionX = function(x) {
+      this.position.x = x;
+    }
+
+    Position.prototype.setPositionY = function(y) {
+      this.position.y = y;
+    }
+
+    Position.prototype.getPositionX = function() {
+      return this.position.x;
+    }
+    Position.prototype.getPositionY = function() {
+      return this.position.y;
     }
 
     function Character(image, position) {
@@ -53,15 +110,19 @@
       Character.prototype.draw.call(this);
     }
 
-    function Keyboard() {
-      var targetElement = document.body;
+    Hero.prototype.isDead = function() {
+      ++this.lives;
+      console.log(this.lives);
+      return this.lives == 80;
+    }
 
-      targetElement.addEventListener('keydown', function (event) {
-           this.keyCode = event.keyCode;
-           console.log(this.keyCode);
-        });
-    }     
-
+    Hero.prototype.move = function(direction) {
+      if (direction == 'right') {
+        Position.prototype.setPositionX.call(this, (Position.prototype.getPositionX.call(this) + 100));
+      } else if (direction == 'left'){
+        Position.prototype.setPositionX.call(this, (Position.prototype.getPositionX.call(this) - 100));
+      }
+    }
 
     $.fn.arcadegame = function(options) {
       return this.each(function() {
